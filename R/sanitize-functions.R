d21d952183a2641b32f818b3a45a81a332dc2c1a
Sebastian Gibb <mail@sebastiangibb.de>
2012-12-02 12:39:50 +0100

    add base64encode function

--- R/sanitize-functions.R
+++ R/sanitize-functions.R
@@ -16,7 +16,6 @@
 ## You should have received a copy of the GNU General Public License
 ## along with MALDIquantForeign. If not, see <http://www.gnu.org/licenses/>
 
-#' sanitize xml strings
 #' @keywords internal
 .sanitize <- function(x) {
   x <- gsub(pattern="&", replacement="&amp;", x=x)
