675c20eb1db9ec110692e9f20c4b487542344c43
Sebastian Gibb <mail@sebastiangibb.de>
2013-01-26 14:21:00 +0100

    remove MALDIquant from Import field

--- R/package.R
+++ R/package.R
@@ -23,8 +23,8 @@
 #'
 #' \tabular{ll}{
 #' Package: \tab MALDIquantForeign \cr
-#' Version: \tab 0.2\cr
-#' Date: \tab 2013-01-14\cr
+#' Version: \tab 0.2.1\cr
+#' Date: \tab 2013-01-26\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab http://strimmerlab.org/software/maldiquant/\cr
 #' }
