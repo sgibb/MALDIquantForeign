245d082330cb4c7f5f3ada869bd763766bfaa98b
Sebastian Gibb <mail@sebastiangibb.de>
2013-02-06 17:23:22 +0100

    add missing usage section in export methods man pages; fixes #1

--- R/package.R
+++ R/package.R
@@ -23,8 +23,8 @@
 #'
 #' \tabular{ll}{
 #' Package: \tab MALDIquantForeign \cr
-#' Version: \tab 0.2.2\cr
-#' Date: \tab 2013-01-27\cr
+#' Version: \tab 0.2.3\cr
+#' Date: \tab 2013-02-06\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab http://strimmerlab.org/software/maldiquant/\cr
 #' }
