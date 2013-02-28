946eb33ef597d392a5aa80aabc9329f6d090b20e
Sebastian Gibb <mail@sebastiangibb.de>
2013-02-28 19:20:32 +0100

    convert tar.bz2 test data into tar.gz because tar on win-builder doesn't support bzip2

--- R/package.R
+++ R/package.R
@@ -23,7 +23,7 @@
 #'
 #' \tabular{ll}{
 #' Package: \tab MALDIquantForeign \cr
-#' Version: \tab 0.2.9\cr
+#' Version: \tab 0.2.9.1\cr
 #' Date: \tab 2013-02-27\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab http://strimmerlab.org/software/maldiquant/\cr
