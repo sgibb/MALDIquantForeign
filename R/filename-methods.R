8eb79772162fb5379777a9d1b2577060c41a703f
Sebastian Gibb <mail@sebastiangibb.de>
2013-04-28 11:55:43 +0200

    fix GPL comment

--- R/filename-methods.R
+++ R/filename-methods.R
@@ -1,24 +1,27 @@
 ## Copyright 2012 Sebastian Gibb
 ## <mail@sebastiangibb.de>
 ##
-## This is free software: you can redistribute it and/or modify
+## This file is part of MALDIquantForeign for R and related languages.
+##
+## MALDIquantForeign is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
 ##
-## It is distributed in the hope that it will be useful,
+## MALDIquantForeign is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
-## See <http://www.gnu.org/licenses/>
+## You should have received a copy of the GNU General Public License
+## along with MALDIquantForeign. If not, see <http://www.gnu.org/licenses/>
 
 #' This method creates a filename for a \code{\linkS4class{AbstractMassObject}}
 #' object.
-#' 
+#'
 #' @param x a \code{\linkS4class{AbstractMassObject}} object
 #' @param fileExtension file type (e.g. "txt", "pdf", ...)
-#' @return filename 
+#' @return filename
 #'
 #' @seealso \code{\linkS4class{AbstractMassObject}}
 #' @keywords internal
@@ -38,7 +41,7 @@ setMethod(f=".composeFilename",
     }
   } else {
     filename <- .withoutFileExtension(metaData(x)$file[1])
-  } 
+  }
 
   filename <- paste(filename, fileExtension, sep=".")
 
