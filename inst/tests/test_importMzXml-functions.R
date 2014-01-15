848fe07043d99573f4ec8eb364a1a2f06f38f149
Sebastian Gibb <mail@sebastiangibb.de>
2014-01-15 15:02:35 +0100

    adjust test cases to centroided mzXML and mzML files

--- inst/tests/test_importMzXml-functions.R
+++ inst/tests/test_importMzXml-functions.R
@@ -10,6 +10,7 @@ test_that("importMzXml", {
   expect_equal(s, importMzXml(path))
   expect_equal(s, import(path, type="mzXML"))
 
+  expect_true(isMassSpectrum(s[[1]]))
   expect_equal(mass(s[[1]]), 1:5)
   expect_equal(intensity(s[[1]]), 6:10)
   expect_equal(basename(metaData(s[[1]])$file), "tiny1.mzXML3.0.mzXML")
@@ -25,9 +26,27 @@ test_that("importMzXml compressed", {
   expect_equal(s, importMzXml(path))
   expect_equal(s, import(path, type="mzXML"))
 
+  expect_true(isMassSpectrum(s[[1]]))
   expect_equal(mass(s[[1]]), 1:5)
   expect_equal(intensity(s[[1]]), 6:10)
   expect_equal(basename(metaData(s[[1]])$file),
                "tiny1-compressed.mzXML3.0.mzXML")
+})
+
+test_that("importMzXml centroided", {
+  expect_error(MALDIquantForeign:::.importMzXml("tmp.tmp"))
+
+  path <- file.path("data", "tiny1-centroided.mzXML3.0.mzXML")
+  p <- MALDIquantForeign:::.importMzXml(path)
+
+  expect_equal(p, import(path))
+  expect_equal(p, importMzXml(path))
+  expect_equal(p, import(path, type="mzXML"))
+
+  expect_true(isMassPeaks(p[[1]]))
+  expect_equal(mass(p[[1]]), 1:5)
+  expect_equal(intensity(p[[1]]), 6:10)
+  expect_equal(basename(metaData(p[[1]])$file),
+               "tiny1-centroided.mzXML3.0.mzXML")
 })
 
