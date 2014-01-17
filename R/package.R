72d3f022cf6be553dbe29450978b38615ecfc736
Sebastian Gibb <mail@sebastiangibb.de>
2014-01-17 16:51:43 +0100

    add ABSiex Analyze files support

--- R/package.R
+++ R/package.R
@@ -23,8 +23,8 @@
 #'
 #' \tabular{ll}{
 #' Package: \tab MALDIquantForeign \cr
-#' Version: \tab 0.5.7\cr
-#' Date: \tab 2014-01-16\cr
+#' Version: \tab 0.5.8\cr
+#' Date: \tab 2014-01-17\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab http://strimmerlab.org/software/maldiquant/\cr
 #' }
