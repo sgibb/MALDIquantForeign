d94f58f55571818f9758d41b39fcf4ad29bfc227
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-11 13:22:24 +0100

    set version to 0.7

--- R/package.R
+++ R/package.R
@@ -23,8 +23,8 @@
 #'
 #' \tabular{ll}{
 #' Package: \tab MALDIquantForeign \cr
-#' Version: \tab 0.6.3\cr
-#' Date: \tab 2014-03-31\cr
+#' Version: \tab 0.7\cr
+#' Date: \tab 2014-04-11\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab http://strimmerlab.org/software/maldiquant/\cr
 #' }
