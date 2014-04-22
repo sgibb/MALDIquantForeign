cdbe366341c2a1c9928a128749169f5209fd75f7
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-22 22:50:06 +0100

    remove version and date information from manual page

--- R/package.R
+++ R/package.R
@@ -23,8 +23,6 @@
 #'
 #' \tabular{ll}{
 #' Package: \tab MALDIquantForeign \cr
-#' Version: \tab 0.7.1\cr
-#' Date: \tab 2014-04-12\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab http://strimmerlab.org/software/maldiquant/\cr
 #' }
