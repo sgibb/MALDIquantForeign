b52b2bbc0b7b7701d66d5e20a72538a25dc0073a
Sebastian Gibb <mail@sebastiangibb.de>
2016-11-29 17:07:54 +0100

    replace deprecated not by expect_silent

--- tests/testthat/test_msg-functions.R
+++ tests/testthat/test_msg-functions.R
@@ -4,5 +4,5 @@ context("msg")
 test_that("msg-functions", {
   expect_message(MALDIquantForeign:::.msg(TRUE, "foobar"), "foobar")
   expect_message(MALDIquantForeign:::.msg(TRUE, "foo", "bar"), "foobar")
-  expect_that(MALDIquantForeign:::.msg(FALSE, "foobar"), not(shows_message()))
+  expect_silent(MALDIquantForeign:::.msg(FALSE, "foobar"))
 })
