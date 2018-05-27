5cf9298a0aa3d376da974b83ad0f42b3aa9fbf91
Sebastian Gibb <mail@sebastiangibb.de>
2018-05-27 15:20:00 +0200

    Add unit test for PR #25

---
+++ tests/testthat/test_mzMl-functions.R
@@ -0,0 +1,19 @@
+context("mzMl")
+
+test_that(".writeImzMlScanList", {
+  m <- createMassSpectrum(mass=1:5, intensity=6:10,
+                          metaData=list(imaging=list(pos=c(x=1e8, y=1e8))))
+  f <- file.path(tempdir(), "imzlscanlist")
+
+  r <- c(
+"    <scanList count=\"1\">",
+"     <scan>",
+"      <cvParam cvRef=\"IMS\" accession=\"IMS:1000050\" name=\"position x\" value=\"100000000\"/>",
+"      <cvParam cvRef=\"IMS\" accession=\"IMS:1000051\" name=\"position y\" value=\"100000000\"/>",
+"     </scan>",
+"    </scanList>"
+)
+
+  MALDIquantForeign:::.writeImzMlScanList(m, file=f)
+  expect_equal(readLines(f), r)
+})
