e917f1f5cb29f25f6e13c8f49fb7ee7f5ed71a46
Sebastian Gibb <mail@sebastiangibb.de>
2025-10-20 20:40:41 +0200

    chore: move to codeberg.org

--- tests/testthat/test_msg-functions.R
+++
@@ -1,8 +0,0 @@
-context("msg")
-
-
-test_that("msg-functions", {
-  expect_message(MALDIquantForeign:::.msg(TRUE, "foobar"), "foobar")
-  expect_message(MALDIquantForeign:::.msg(TRUE, "foo", "bar"), "foobar")
-  expect_silent(MALDIquantForeign:::.msg(FALSE, "foobar"))
-})
--- tests/testthat/test_sanitize-functions.R
+++
@@ -1,8 +0,0 @@
-context("sanitize")
-
-
-test_that("sanitize-functions", {
-  s <- c("a<b", "b>c", "a&o", "abc")
-  r <- c("a&lt;b", "b&gt;c", "a&amp;o", "abc")
-  expect_identical(MALDIquantForeign:::.sanitize(s), r)
-})
